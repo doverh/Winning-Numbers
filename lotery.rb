38907ebfeea43659313eec23cbc001fac723379c
doverh <doverheld@gmail.com>
2017-01-27 15:44:46 -0500

    Updating winning numbers

--- lotery.rb
+++ lotery.rb
@@ -2,28 +2,28 @@
 # Write a function that takes a number (our ticket number) and an array of numbers (the winning numbers from all the draws). To start with, just restrict to all 4 digit numbers. Having the numbers as strings may make it easier to solve. The function should return an array of all the winning numbers that are one digit off from our ticket number.
 # eg our_number = '1234' and winning_numbers = ['9999', '5678', '1235', '1134', '1344'] should return ['1235', '1134']
 
-def run_lotery(our_num,*lotery)
+def run_numbers(our_num,*array_numbers)
 	#Define a variable to receive the winners
 	winners = []
 	
-	lotery.each do |value|
+	array_numbers.each do |value|
 		#For each char of the number compare with our number	
 		puts value
-		result= 0
+		result = 0
 		for num in 0..3
 			if our_num[num] == value[num]
 				result+=1
 			end
-			if result >= 3
-				winners.push(value)
+		end
+		#Check if our number has more than 3 equals chars from numbers
+		if result >= 3
+			winners.push(value)
 				#Set again variable to zero to count next
-				result = 0
-				print winners
-			end
-
+				#result = 0
+			print winners
 		end	
 	end
 	return winners
 
 end
-#run_lotery('1234','9999', '5678', '1134', '1344')
+
--- test_lotery.rb
+++ test_lotery.rb
@@ -3,8 +3,17 @@ require_relative "lotery.rb"
 
 class TestLottery < Minitest::Test
 	
-	def test_check_my_numbers_won
-		assert_equal(['1235', '1134'], run_lotery('1234','9999', '5678', '1235', '1134', '1344'))
+	# def test_check_my_numbers_won
+	# 	assert_equal(['1235', '1134'], run_numbers('1234',['9999', '5678', '1235', '1134', '1344'))
+	# end
+
+	# def test_check_my_numbers_won_version_2
+	# 	assert_equal(['1234'], run_numbers('1234','1234', '5678', '0000', '1199', '1344'))
+	# end
+
+	def test_1234_first
+		assert_equal(['1234','1333','1133'], run_numbers('1233','1234','1333','5555','1133'))		
 	end
+
 
 end
