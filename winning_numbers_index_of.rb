628421b22f484412bb25a4746a5c0bb9b35ac109
doverh <doverheld@gmail.com>
2017-01-27 09:15:25 -0500

    Adding comments

--- winning_numbers_index_of.rb
+++ winning_numbers_index_of.rb
@@ -1,7 +1,12 @@
 
 def win_number(num,*lotery)
+	
+	#Create variable to determine what is the index of the winner
 	index = 0
+
+	#For each number in the array verify if is the WINNER num
 	lotery.each do |value|
+	#Use include to determine that lotery(value) include our number
 	if value.include?(num)
 		puts lotery.index(num)	
 		return true
