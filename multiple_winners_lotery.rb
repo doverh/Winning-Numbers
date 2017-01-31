e88a6379c7f90a05a3d0847cb3f965643e40261e
doverh <doverheld@gmail.com>
2017-01-31 11:17:57 -0500

    Uploading new exercise

--- multiple_winners_lotery.rb
+++ multiple_winners_lotery.rb
@@ -2,30 +2,47 @@
 # who won what? who was off by one? which prizes were not claimed? 
 # 3 functions each function inputs 2 arrays outputs 1
 
-def run_numbers(our_num,array_numbers)
+
+#This function lotery numbers by comparing numbers you have choosen with drawn numbers  
+def run_numbers(numbers,array_numbers)
 	#Define a variable to receive the winners
 	winners = []
 	
-	array_numbers.each do |value|
-		#For each char of the number compare with our number	
-		puts value
-		result = 0
-		for num in 0..3
-			if our_num[num] == value[num]
-				result+=1
-			end
+	numbers.each do |number|
+	
+		array_numbers.each do |value|
+			#For each char of the number compare with our number	
+			result = 0
+				for num in 0..(array_numbers.length-1)
+					result+=1 if number[num] == value[num]		
+				end
+				#Check if our number has more than 3 equals chars from numbers
+				winners.push(value) if result == 3			
 		end
-		#Check if our number has more than 3 equals chars from numbers
-		if result >= 3
-			winners.push(value)
-				#Set again variable to zero to count next
-				#result = 0
-			print winners
-		end	
 	end
+	print winners
 	return winners
 
 end
 
-def prizes(winners,)
+run_numbers(['1233','5855'],['1234','1333','5555','1593'])
+
+def prizes_winners(winners,prizes)
+	
+	results = []
+
+	#multi_array = names.shuffle.each_slice(2)
+	
+		for num in (0..prizes.length-1)
+			if !winners[num].nil?
+			 	results.push(winners[num],prizes[num])
+			else
+				results.push("",prizes[num])
+			end
+		end
+		print_results = results.each_slice(2).to_a
+		print print_results
+		return print_results
+			
+end
 
--- test_multiple_winners_lotery.rb
+++ test_multiple_winners_lotery.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require_relative "lotery.rb"
+require_relative "multiple_winners_lotery.rb"
 
 class TestLottery < Minitest::Test
 	
@@ -11,9 +11,18 @@ class TestLottery < Minitest::Test
 	# 	assert_equal(['1234'], run_numbers('1234','1234', '5678', '0000', '1199', '1344'))
 	# end
 
-	def test_1234_first
-		assert_equal(['1234','1333','1133'], run_numbers('1233','1234','1333','5555','1133'))		
+	def test_passing_array_number_return_array_winners
+		assert_equal(['1234','1333','5555'], run_numbers(['1233','5855'],['1234','1333','5555','1593']))		
 	end
+
+	def test_prizes_for_the_winners
+		winners = run_numbers(['1233','5855'],['1234','1333','5555','1593'])
+		prizes = ['1000000.00','500000.00','100000.00', '50000.00']
+		assert_equal([['1234','1000000.00'],['1333','500000.00'],['5555','100000.00'],['','50000.00']], prizes_winners(winners,prizes))
+	end
+
+	# def test_prizzes_not_claimed
+
 
 
 end
